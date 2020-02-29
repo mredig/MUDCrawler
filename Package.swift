113f2482490e791c38dcfe8d537e32be72687ab5
Michael Redig <mredig@gmail.com>
2020-02-29 01:58:52 -0600

    (feat) use updated NetworkHandler as SPM

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
 	dependencies: [
 		// Dependencies declare other packages that this package depends on.
 		// .package(url: /* package url */, from: "1.0.0"),
-		.package(path: "../NetworkHandlerSPM/"),
-		.package(url: "https://github.com/johnsundell/files.git", from: "4.0.0")
+		.package(url: "https://github.com/johnsundell/files.git", from: "4.0.0"),
+		.package(url: "https://github.com/mredig/NetworkHandler.git", from: "0.9.7")
 	],
 	targets: [
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,7 +23,7 @@ let package = Package(
 		.target(name: "MUDCrawlerCore",
 				dependencies: [
 					"Files",
-					"NetworkHandlerSPM"
+					"NetworkHandler"
 		]),
 		.target(
 			name: "MUDCrawler",
--- Sources/MUDCrawlerCore/MUDCrawler.swift
+++ Sources/MUDCrawlerCore/MUDCrawler.swift
@@ -1,5 +1,5 @@
 import Foundation
-import NetworkHandlerSPM
+import NetworkHandler
 
 public struct MUDCrawler {
 
