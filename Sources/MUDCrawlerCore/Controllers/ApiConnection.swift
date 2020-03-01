3882e468f9fbdbf773f3897a6dc6126cdb24f8af
Michael Redig <mredig@gmail.com>
2020-03-01 01:50:32 -0600

    (refactor) revised how request headers are generated
    added move player api call

--- Sources/MUDCrawlerCore/Controllers/ApiConnection.swift
+++ Sources/MUDCrawlerCore/Controllers/ApiConnection.swift
@@ -17,11 +17,11 @@ class ApiConnection {
 		self.token = token
 	}
 
-	var requestHeaders: [String: String]? {
-		var request = baseURL.request
+	func getRequest(from url: URL) -> NetworkRequest {
+		var request = url.request
 		request.addValue(.contentType(type: .json), forHTTPHeaderField: .commonKey(key: .contentType))
 		request.addValue(.other(value: "Token \(token)"), forHTTPHeaderField: .commonKey(key: .authorization))
-		return request.allHeaderFields
+		return request
 	}
 
 	func initPlayer(completion: @escaping (Result<RoomResponse, NetworkError>) -> Void) {
@@ -29,9 +29,18 @@ class ApiConnection {
 			.appendingPathComponent("adv", isDirectory: true)
 			.appendingPathComponent("init", isDirectory: true)
 
-		var request = url.request
-		request.allHeaderFields = requestHeaders
+		let request = getRequest(from: url)
 
+		NetworkHandler.default.transferMahCodableDatas(with: request, completion: completion)
+	}
+
+	func movePlayer(direction: Direction, completion: @escaping (Result<RoomResponse, NetworkError>) -> Void) {
+		let url = baseURL.appendingPathComponent("api", isDirectory: true)
+			.appendingPathComponent("adv", isDirectory: true)
+			.appendingPathComponent("move", isDirectory: true)
+
+		var request = getRequest(from: url)
+		request.encodeData(DirectionWrapper(direction: direction))
 		NetworkHandler.default.transferMahCodableDatas(with: request, completion: completion)
 	}
 }
